a2a6379b3ef342ae33e9afa7e95c666332f9d90e
Stefan <5796872+stefandango@users.noreply.github.com>
2023-01-18 20:52:53 +0100

    changes to waybar

---
+++ scripts/.local/bin/waybar-diskinfo.sh
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env sh
+
+df -Ph "$HOME" | tail -n 1 |
+  jq --compact-output --unbuffered -R -s '
+    gsub(" +"; " ") | split(" ") | {
+      mount: .[0],
+      total: .[1],
+      used: .[2],
+      free: .[3],
+      percentage_used: .[4],
+      text: .[4],
+      tooltip: "\(.[2])B / \(.[1])B"
+    }'
