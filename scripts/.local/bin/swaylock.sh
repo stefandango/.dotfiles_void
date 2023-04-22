edd02454a314cbe379a842540ee1607fbc6cd1f9
Stefan <5796872+stefandango@users.noreply.github.com>
2023-04-22 22:44:12 +0200

    added scripts for swaylock (effects) and swayidle

---
+++ scripts/.local/bin/swaylock.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+swaylock --screenshots \
+            --clock \
+            --indicator \
+            --indicator-radius 100 \
+            --indicator-thickness 7 \
+            --effect-blur 7x5 \
+            --effect-vignette 0.5:0.5 \
+            --ring-color 192330 \
+            --key-hl-color 9d79d6 \
+            --line-color 000000 \
+            --inside-color c94f6d \
+            --separator-color 000000 \
+            --grace 3 \
+            --fade-in 0.5 \
+            --effect-greyscale -d
