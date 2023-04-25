729d792416054a79df1f68483e45cb29ef5906f7
Stefan <5796872+stefandango@users.noreply.github.com>
2023-04-25 22:05:52 +0200

    added nvim dashboard

--- nvim/.config/nvim/lua/stefandango/packer.lua
+++ nvim/.config/nvim/lua/stefandango/packer.lua
@@ -124,6 +124,7 @@ require('packer').startup(function(use)
             }
         end
     }
+    use('glepnir/dashboard-nvim') 
 
     -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
     local has_plugins, plugins = pcall(require, 'custom.plugins')
