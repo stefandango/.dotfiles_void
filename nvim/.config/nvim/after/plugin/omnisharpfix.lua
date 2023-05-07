33517233fc6f3e51b04d19add1086132d5a06236
Stefan <5796872+stefandango@users.noreply.github.com>
2023-05-07 20:07:06 +0200

    quickfix for omnisharp in nvim

---
+++ nvim/.config/nvim/after/plugin/omnisharpfix.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "Fix startup error by disabling semantic tokens for omnisharp",
+  group = vim.api.nvim_create_augroup("OmnisharpHook", {}),
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client.name == "omnisharp" then
+      client.server_capabilities.semanticTokensProvider = nil
+    end
+  end,
+})
