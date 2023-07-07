32bbe58356ad02bd7b8f07eabc41367e50a14b01
Stefan <5796872+stefandango@users.noreply.github.com>
2023-07-07 22:24:48 +0200

    added override for missing icons in webicons nvim

---
+++ nvim/.config/nvim/after/plugin/iconsoverride.lua
@@ -0,0 +1,18 @@
+require'nvim-web-devicons'.setup {
+    override = {
+        cs = {
+            icon = "",
+            name = "Cs",
+            color = "#596706",
+            cterm_color = "58",
+        },
+        txt = {
+            icon = ""
+        },
+        csproj = {
+            icon = "",
+            color = "#9400D3"
+        }
+    }
+
+}
