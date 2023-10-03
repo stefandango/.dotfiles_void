61f9b308d51c7cdbcf4676ba81539c0641227905
Stefan <5796872+stefandango@users.noreply.github.com>
2023-10-03 21:10:50 +0200

    updated vscode update script with nice spinner

--- scripts/.local/bin/vscode-update.py
+++ scripts/.local/bin/vscode-update.py
@@ -1,11 +1,44 @@
-#!/bin/env python
+#!/usr/bin/env python
 import requests
 import tarfile
+import sys
+import time
+
+def print_spinner(message):
+    spin_chars = "|/-\\"
+    for _ in range(10):  # Adjust the number of iterations as needed
+        for char in spin_chars:
+            sys.stdout.write(f"\r{message}... {char}")
+            sys.stdout.flush()
+            time.sleep(0.1)
+
+def print_colored_message(message, color):
+    colors = {
+        "reset": "\033[0m",
+        "green": "\033[92m",
+    }
+    colored_message = f"{colors[color]}{message}{colors['reset']}"
+    print(colored_message)
 
 url = "https://code.visualstudio.com/sha/download?build=stable&os=linux-x64"
 
 response = requests.get(url, stream=True)
-file = tarfile.open(fileobj=response.raw, mode="r|gz")
-file.extractall(path="/home/stefan/Scripts/")
-file.close()
+
+if response.status_code == 200:
+    print_spinner("Downloading")
+    sys.stdout.write("\rDownloading... Done!\n")
+    sys.stdout.flush()
+    
+    file = tarfile.open(fileobj=response.raw)
+    
+    print_spinner("Extracting")
+    file.extractall(path="/home/stefan/Scripts/")
+    file.close()
+    
+    sys.stdout.write("\rExtracting... Done!\n")
+    sys.stdout.flush()
+    
+    print_colored_message("Script completed successfully!", "green")
+else:
+    print("Failed to download the file.")
 
