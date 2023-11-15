c1d960df9d4edcabba976f0dc0a89a635eaddbbd
Stefan <5796872+stefandango@users.noreply.github.com>
2023-11-15 14:14:18 +0100

    updated init of blankline plugin to match new version

--- nvim/.config/nvim/after/plugin/blankline.lua
+++ nvim/.config/nvim/after/plugin/blankline.lua
@@ -1,10 +1,13 @@
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
-vim.opt.list = true
-vim.opt.listchars:append "eol:↴"
+--vim.opt.list = true
+--vim.opt.listchars:append "eol:↴"
+--
+-- require('indent_blankline').setup {
+-- 	char = '┊',
+-- 	show_trailing_blankline_indent = false,
+-- 	show_end_of_line = true
+-- }
+--
 
-require('indent_blankline').setup {
-	char = '┊',
-	show_trailing_blankline_indent = false,
-	show_end_of_line = true
-}
+require("ibl").setup()
